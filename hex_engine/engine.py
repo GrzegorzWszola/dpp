73d3572677217adbd373532567ec29075537e38e
GrzegorzWszola <grzegorzwszola1@gmail.com>
2026-04-27 18:08:35 +0200

    first commit

---
+++ hex_engine/engine.py
@@ -0,0 +1,16 @@
+from player import *
+from board import *
+
+class Engine:
+    def __init__(self,
+                 player,
+                 player_2 = AIPlayer(),
+                 size = 6):
+        self.players = [player, player_2]
+        self.board = Board(size)
+
+    def start():
+        return
+    
+    def make_move():
+        return
---
+++ hex_engine/player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self):
+        return
+    
+    def get_move():
+        return
+    
+class HumanPlayer(Player):
+    def get_move():
+        pass
